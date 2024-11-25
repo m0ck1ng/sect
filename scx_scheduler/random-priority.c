38276d364512c9d61df0234af3b75a86ecafbe29
m0ck1ng <stitch@zju.edu.cn>
2024-11-25 19:35:09 +0800

    feat: update scheduling algorithms

--- scx_scheduler/random-priority.c
+++ scx_scheduler/random-priority.c
@@ -1,5 +1,4 @@
-
-/* 
+/*
  * Random Priority Walk implementation. @use_random_walk is set in the corresponding
  * C program based on user-defined parameters.
  */
@@ -9,21 +8,21 @@
  */
 static inline int update_priorities_rp(pid_t pid)
 {
-	u32 priority = bpf_get_prandom_u32() % 50 + 1;
-	// s32 priority = xorshift32(&rng_state) % 2147483647;
-	dbg("[update_priorities_rp] prio new %d", priority);
+        u32 priority = bpf_get_prandom_u32() % 50 + 1;
+        // s32 priority = xorshift32(&rng_state) % 2147483647;
+        dbg("[update_priorities_rp] prio new %d", priority);
 
-	if (priority < 0) {
-		bpf_printk("[enqueue] failed to assign priority for pid: %d, priority: %d\n",
-		     pid, priority);
-		return -1;
-	}
-	
-	update_priority(pid, priority);
+        if (priority < 0) {
+                bpf_printk("[enqueue] failed to assign priority for pid: %d, priority: %d\n",
+                     pid, priority);
+                return -1;
+        }
 
-	return 0;
+        update_priority(pid, priority);
+
+        return 0;
 }
 
 static inline s32 init_rp() {
-	  return 0;
+          return 0;
 }
