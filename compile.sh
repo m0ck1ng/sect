a9813059c24494989c2b0ab95bc748ec580976ea
dylan <wolffdy0@gmail.com>
2025-07-07 08:30:26 +0000

    updating compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,24 @@
-# customize kernel config here
-V=17
+#!/bin/bash
+sudo apt install -y \
+  build-essential \
+  bc \
+  flex \
+  bison \
+  libssl-dev \
+  libelf-dev \
+  libncurses-dev \
+  dwarves \
+  pahole \
+  libudev-dev \
+  libpci-dev \
+  libiberty-dev \
+  python3 \
+  rsync \
+  zstd
+
+
+# note clang must be installed manually
+V=16
 make CC=clang-$V olddefconfig
 make CC=clang-$V \
     LD=ld.lld-$V \
